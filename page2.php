9252fb0f474f9f2bcb5558ddd9ab7dc3c4db8763
Davor Kedacic <dkedacic@gmail.com>
2016-10-18 19:40:42 +0200

    initial commit

---
+++ page2.php
@@ -0,0 +1,31 @@
+
+
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        
+        <div>TODO write content
+        <h1>php akademija</h1>
+        <img src="https://s22.postimg.org/e6mt8mdfl/IMG_B46_CB9_746101_436299_89_DB06_448_D94_A5_AFFE_1.jpg"/>
+        <?php if(date('d.m.Y')== '18.10.2016'): ?>
+        <p> phpacademy star's</p>
+        <?php endif ?>
+        <footer><p>date: <?php echo date('d.m.Y') == '18.10.2016.') ?><p></footer>  
+        </p>
+        
+        </div>
+    </body>
+</html>
+        
+        
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
